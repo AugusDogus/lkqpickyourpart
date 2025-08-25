bfa13769b1fd020e5a19b4e071e875084ee7aa4b
AugusDogus <9794679+AugusDogus@users.noreply.github.com>
2025-08-25 12:17:13 -0500

    chore: remove unused post router
    - Remove postRouter import from root router
    - Remove post router from app router configuration
    - Delete unused post.ts file
    - Clean up development-only code

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,4 @@
 import { locationsRouter } from "~/server/api/routers/locations";
-import { postRouter } from "~/server/api/routers/post";
 import { vehiclesRouter } from "~/server/api/routers/vehicles";
 import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
 
@@ -9,7 +8,6 @@ import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  post: postRouter,
   locations: locationsRouter,
   vehicles: vehiclesRouter,
 });
